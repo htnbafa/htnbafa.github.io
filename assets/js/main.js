e762762bf3ca54c2c199246e9a1a5c476ae389fb
htnbafa <37487104+htnbafa@users.noreply.github.com>
2018-03-17 16:14:36 -0700

    accordion js

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,7 +109,24 @@
 					.on('-large', function() {
 						$intro.prependTo($sidebar);
 					});
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
 	});
 
 })(jQuery);
